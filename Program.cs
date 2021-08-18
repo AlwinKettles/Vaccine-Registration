b4ae833a3d99816e4fd00bfea9b0b196362ce109
AlwinKettles <alwinkettles@gmail.com>
2021-08-18 09:52:47 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaccine_Registration
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Vaccine registration system.");
+            Console.WriteLine("Please wait!");
+            Console.WriteLine("Enter your SA ID:");
+            string userID = Console.ReadLine();
+
+            Console.WriteLine("Enter your Full Name:");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your Surname:");
+            string userSurname = Console.ReadLine();
+
+            Console.WriteLine("Enter your Provice:");
+            string Province = Console.ReadLine();
+
+            Console.WriteLine("Have you been in contact with any covid cases? (Awnser 'Y' or 'N')");
+            string contact = Console.ReadLine();
+
+            Console.WriteLine("Do you have any of the covid 19 symptons? (Awnser 'Y' or 'N')");
+            string symptons = Console.ReadLine();
+
+            if (contact == "Y" || contact == "y" || symptons == "Y" || symptons == "y")
+            {
+                Console.WriteLine("Full Name: " + userName + " SA ID " + userID + " Surname: " + userSurname + " Province: " + Province + " YOU MIGHT HAVE COVID 19" );
+            }
+            else
+            {
+                Console.WriteLine("Full Name: " + userName + " SA ID " + userID + " Surname: " + userSurname + " Province: " + Province + ", You do not have covid 19");
+            }
+
+
+            Console.Read();
+            
+        }
+    }
+}
